ee9687302b3b72f34f04a28de18fa14793d3b8d4
riadeb <51858603+riadeb@users.noreply.github.com>
2020-07-28 23:44:32 +0100

    Added status request

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ async function handleRequest(request) {
 
     r.get('/code/.*', handleCodeRequest);
     r.get('/risk/.*', handleRiskRequest);
+    r.get('/status/.*', handleStatusRequest)
 
     const resp = await r.route(request)
     return resp
@@ -58,4 +59,36 @@ async function handleRiskRequest(request) {
   };
 
   return new Response(JSON.stringify(answer), {status: 200})
+}
+
+async function handleStatusRequest(request) {
+    
+    const countryCode = request.url.substring(request.url.lastIndexOf('/') + 1);
+    const indicators_ids = "/2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,3001,3002,3003,3004,3005,3006,3007,3008,3009,3010,4001,4002,4003,4004,4005,4006,4007,4008,4009,4010";
+  
+    var response;
+    if (countryCode == "help"){
+          response = await fetch(`https://reopen.europa.eu/api/covid/v1/eutcdata/data/en/FRA/${indicators_ids}`);
+    }
+    else{
+          response = await fetch(`https://reopen.europa.eu/api/covid/v1/eutcdata/data/en/${countryCode}/${indicators_ids}`);
+    }
+    
+  const body = await response.json();
+    
+  var res = {};
+  var keys = ""
+  
+  body[0]["indicators"].forEach(function(indicator){
+      
+        res[indicator["indicator_name"]] = {"value": indicator["value"], "comment" : indicator["comment"] }
+        keys += indicator["indicator_name"] + "<br>";
+})
+    if (countryCode == "help"){
+  return new Response("<html>"+ keys+'</html>', {status: 200,     headers: { "Content-Type": "text/html" },})
+    }
+    else {
+          return new Response(JSON.stringify(res), {status: 200})
+
+    }
 }
